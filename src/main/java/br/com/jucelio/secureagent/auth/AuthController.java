1c99ba85640d94de6f485f929b34fd9c8f25352a
Jucelio Farias Coelho <juceliocoelho2022@gmail.com>
2026-09-13 15:17:09 -0300

    chore: trace login flow safely

--- src/main/java/br/com/jucelio/secureagent/auth/AuthController.java
+++ src/main/java/br/com/jucelio/secureagent/auth/AuthController.java
@@ -1,6 +1,8 @@
 package br.com.jucelio.secureagent.auth;
 
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
@@ -8,6 +10,8 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/v1/auth")
 public class AuthController {
+    private static final Logger log = LoggerFactory.getLogger(AuthController.class);
+
     private final AuthenticationManager authenticationManager;
     private final TokenService tokens;
 
@@ -18,9 +22,13 @@ public class AuthController {
 
     @PostMapping("/login")
     TokenResponse login(@Valid @RequestBody LoginRequest request) {
+        log.info("AUTH_DIAG step=controller_enter username={}", request.username());
         var auth = authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(request.username(), request.password()));
-        return tokens.issue(auth.getName());
+        log.info("AUTH_DIAG step=manager_success username={}", auth.getName());
+        TokenResponse response = tokens.issue(auth.getName());
+        log.info("AUTH_DIAG step=token_issued username={}", auth.getName());
+        return response;
     }
 
     @PostMapping("/refresh")
